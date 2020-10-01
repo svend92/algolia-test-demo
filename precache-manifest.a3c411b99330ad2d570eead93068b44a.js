9846851ad48e72f4ba47fedf7e2e16b098711987
root <root@DESKTOP-3L6UPTS.localdomain>
2020-10-01 20:02:20 +0200

    Updates

---
+++ precache-manifest.a3c411b99330ad2d570eead93068b44a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6feb4a5cabc173ca2da698d2495154a1",
+    "url": "/algolia-test-demo/index.html"
+  },
+  {
+    "revision": "f60db44d57e5bb3b5175",
+    "url": "/algolia-test-demo/static/css/main.18497b71.chunk.css"
+  },
+  {
+    "revision": "17ecf491afc09136b804",
+    "url": "/algolia-test-demo/static/js/2.33f8dd3c.chunk.js"
+  },
+  {
+    "revision": "059d27417fec403f2221b60b460e5ac3",
+    "url": "/algolia-test-demo/static/js/2.33f8dd3c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f60db44d57e5bb3b5175",
+    "url": "/algolia-test-demo/static/js/main.748c32e3.chunk.js"
+  },
+  {
+    "revision": "5178f9a1e9cefef03a6c",
+    "url": "/algolia-test-demo/static/js/runtime-main.a23f7ff0.js"
+  }
+]);
